729eaae6c65ad3074d3d2cd3026b8d33daa2ba8d
Traxx186 <justinvanderkruit@icloud.com>
2023-12-05 22:26:23 +0100

    feat(frontend): added indeterminate checkbox function

--- frontend/src/components/checkbox.rs
+++ frontend/src/components/checkbox.rs
@@ -6,7 +6,8 @@ use yew::{
 };
 
 pub struct Checkbox {
-    checked: bool
+    checked: bool,
+    indeterminate: bool
 }
 
 #[derive(Clone, PartialEq, Properties)]
@@ -15,6 +16,8 @@ pub struct CheckboxProps {
     pub onchange: Callback<Event>,
     #[prop_or_default]
     pub checked: bool,
+    #[prop_or_default]
+    pub indeterminate: bool,
     #[prop_or_default]
     pub class: String,
 }
@@ -29,8 +32,16 @@ impl Component for Checkbox {
 
     fn create(ctx: &Context<Self>) -> Self {
         let checked = ctx.props().checked;
+        let indeterminate = ctx.props().indeterminate;
 
-        Checkbox { checked }
+        Checkbox { checked, indeterminate }
+    }
+
+    fn changed(&mut self, ctx: &Context<Self>, _old_props: &Self::Properties) -> bool {
+        self.checked = ctx.props().checked;
+        self.indeterminate = ctx.props().indeterminate;
+
+        true
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
@@ -50,7 +61,7 @@ impl Component for Checkbox {
         let mut base_class = vec!["inline-flex", "items-center", "justify-center", "relative", "box-border", "bg-transparent", "outline-0", "border-0", "m-0", "cursor-pointer", "select-none", "align-middle", "appearance-none", "no-underline", "p-2", "rounded-half"];
         let class = ctx.props().class.clone();
 
-        if self.checked {
+        if self.checked || self.indeterminate {
             base_class.append(&mut vec!["text-blue-300"])
         } else {
             base_class.append(&mut vec!["text-white/70"])
@@ -67,6 +78,8 @@ impl Component for Checkbox {
                 <svg class={classes!("select-none", "w-[1em]", "h-[1em]", "inline-block", "fill-current", "shrink-0", "text-2xl", "transition-colors")} focusable="false" aria-hidden="true" viewBox="0 0 24 24">
                     {if self.checked {
                         html!(<path d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path>)
+                    } else if self.indeterminate {
+                        html!(<path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 10H7v-2h10v2z"></path>)
                     } else {
                         html!(<path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"></path>)
                     }}
