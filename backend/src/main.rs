7e336f623fe99a83a347655f8ebe342ab8e6205a
Traxx186 <justinvanderkruit@icloud.com>
2024-08-17 10:44:18 +0200

    chore: removed unused oauth2 dependency

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -15,10 +15,7 @@ use actix_web::{
     rt::{spawn, time::interval},
     web, App, HttpServer,
 };
-use config::{
-    db::{init_db_pool, run_migration},
-    oauth2::OAuth2Client,
-};
+use config::db::{init_db_pool, run_migration};
 use controller::api::{
     game::{self, GameResponseBody, GamesResponseBody},
     level::{self, LevelResponseBody, LevelsResponseBody},
@@ -84,8 +81,6 @@ async fn main() -> std::io::Result<()> {
     let app_port = env::var("APP_PORT").expect("APP_PORT must be set");
     let app_url = format!("{}:{}", app_host, app_port);
 
-    let oauth2_client = OAuth2Client::new().await;
-
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let db_pool = init_db_pool(&db_url);
     run_migration(&mut db_pool.get().unwrap());
@@ -101,7 +96,6 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(db_pool.clone()))
-            .app_data(web::Data::new(oauth2_client.clone()))
             .wrap(Logger::default())
             .wrap(setup_cors())
             .service(controller::api_scope())
