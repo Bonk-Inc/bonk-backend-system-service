6baa51bc4b9896519f1e9c3ef72b5dfcb334c16c
Traxx186 <justinvanderkruit@icloud.com>
2024-10-26 16:28:00 +0200

    feat: added user api endpoint

--- backend/src/models/user.rs
+++ backend/src/models/user.rs
@@ -23,12 +23,12 @@ pub struct User {
 
 #[derive(Insertable, AsChangeset, Serialize, Deserialize, ToSchema)]
 #[diesel(table_name = user)]
-pub struct ScoreDTO {
+pub struct UserDTO {
     pub name: String,
     pub game_id: Uuid,
 }
 
-impl Model<User, Uuid, ScoreDTO> for User {
+impl Model<User, Uuid, UserDTO> for User {
     fn find_all(conn: &mut Connection) -> QueryResult<Vec<User>> {
         user.load::<User>(conn)
     }
@@ -37,13 +37,13 @@ impl Model<User, Uuid, ScoreDTO> for User {
         user.find(user_id).get_result::<User>(conn)
     }
 
-    fn insert(data: ScoreDTO, conn: &mut Connection) -> QueryResult<User> {
+    fn insert(data: UserDTO, conn: &mut Connection) -> QueryResult<User> {
         diesel::insert_into(user)
             .values(&data)
             .get_result::<User>(conn)
     }
 
-    fn update(user_id: Uuid, data: ScoreDTO, conn: &mut Connection) -> QueryResult<User> {
+    fn update(user_id: Uuid, data: UserDTO, conn: &mut Connection) -> QueryResult<User> {
         diesel::update(user)
             .filter(id.eq(user_id))
             .set(data)
@@ -55,4 +55,14 @@ impl Model<User, Uuid, ScoreDTO> for User {
             .filter(id.eq(user_id))
             .execute(conn)
     }
+}
+
+pub fn find_by_game(
+    game: Uuid,
+    conn: &mut Connection,
+) -> QueryResult<Vec<User>> {
+    user
+        .filter(game_id.eq(game))
+        .select(User::as_select())
+        .load(conn)
 }
---
+++ backend/src/service/user_service.rs
@@ -0,0 +1,73 @@
+use uuid::Uuid;
+
+use crate::{
+    config::db::Pool,
+    error::{internal_error, not_found_error, ErrorResponse},
+    models::{user::{self, User, UserDTO}, Model},
+};
+
+use super::game_service;
+
+pub fn find_by_game(game_id: Uuid, pool: &Pool) -> Result<Vec<User>, ErrorResponse> {
+    if !game_service::game_exisits(game_id, pool) {
+        return Err(not_found_error(format!(
+            "Game with id '{}' not found",
+            game_id.to_string()
+        )));
+    }
+
+    match user::find_by_game(game_id, &mut pool.get().unwrap()) {
+        Ok(users) => Ok(users),
+        Err(_) => Err(internal_error("Cannot fetch users".to_string())),
+    }
+}
+
+pub fn find_by_id(id: Uuid, pool: &Pool) -> Result<User, ErrorResponse> {
+    match User::find_by_id(id, &mut pool.get().unwrap()) {
+        Ok(game) => Ok(game),
+        Err(_) => Err(not_found_error(format!("User with id '{}' not found", id.to_string()),)),
+    }
+}
+
+pub fn insert(new_score: UserDTO, pool: &Pool) -> Result<User, ErrorResponse> {
+    match User::insert(new_score, &mut pool.get().unwrap()) {
+        Ok(score) => Ok(score),
+        Err(err) => Err(internal_error(format!("Error saving new user, {}", err))),
+    }
+}
+
+pub fn update(
+    id: Uuid,
+    updated_level: UserDTO,
+    pool: &Pool,
+) -> Result<User, ErrorResponse> {
+    if !user_exists(id, pool) {
+        return Err(not_found_error(format!(
+            "User with id '{}' not found",
+            id.to_string()
+        )));
+    }
+
+    match User::update(id, updated_level, &mut pool.get().unwrap()) {
+        Ok(level) => Ok(level),
+        Err(_) => Err(internal_error("Could not update user".to_string())),
+    }
+}
+
+pub fn delete(id: Uuid, pool: &Pool) -> Result<usize, ErrorResponse> {
+    if !user_exists(id, pool) {
+        return Err(not_found_error(format!(
+            "User with id '{}' not found",
+            id.to_string()
+        )));
+    }
+
+    match User::delete(id, &mut pool.get().unwrap()) {
+        Ok(results) => Ok(results),
+        Err(_) => Err(internal_error("Could not delete user".to_string())),
+    }
+}
+
+pub fn user_exists(id: Uuid, pool: &Pool) -> bool {
+    User::find_by_id(id, &mut pool.get().unwrap()).is_ok()
+}
