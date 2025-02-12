64cc03694383cd27135eecf4db15f61d7eb866db
Traxx186 <justinvanderkruit@icloud.com>
2025-02-12 21:32:47 +0100

    fix: upgrade issues tailwind v4

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,16 +2,10 @@ const animate = require("tailwindcss-animate")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
-  safelist: ["dark"],
-  prefix: "",
-  
   content: [
-    './pages/**/*.{ts,tsx,vue}',
-    './components/**/*.{ts,tsx,vue}',
-    './app/**/*.{ts,tsx,vue}',
+    './**/*.html',
     './src/**/*.{ts,tsx,vue}',
-	],
+  ],
   
   theme: {
     container: {
