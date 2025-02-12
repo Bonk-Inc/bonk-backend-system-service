1fd42a8c1e8ddae3efcc475239a3e20897843142
Traxx186 <justinvanderkruit@icloud.com>
2025-02-12 21:32:47 +0100

    fix: upgrade issues tailwind v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,21 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()]
+    plugins: [
+        tailwindcss(),
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        port: 5174
     }
-  },
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    port: 5174
-  }
 });
